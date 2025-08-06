a007819db7bb9d70eb8c1a6313b91a28e20e7643
Hải Yến <haiiyenn210@gmail.com>
2025-08-06 11:51:45 +0700

    .

--- QLSRM.BL/BLDailyOrder.cs
+++ QLSRM.BL/BLDailyOrder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QLSRM.DL;
+using QLSRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace QLSRM.BL
 {
     public class BLDailyOrder : BLBase
     {
+        public DLDailyOrder _dlDaiLyOrder { get; set; }
+        public BLDailyOrder()
+        {
+            _dlDaiLyOrder = new DLDailyOrder();
+        }
+        public List<DailyOrder> GetDailyOrderByCustomer(long customerId)
+        {
+            return _dlDaiLyOrder.GetDailyOrderByCustomer(customerId);
+        }
+
+        
     }
 }
--- QLSRM.DL/DLDailyOrder.cs
+++ QLSRM.DL/DLDailyOrder.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using QLSRM.Common;
+using QLSRM.Library;
+using QLSRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,18 @@ namespace QLSRM.DL
 {
     public class DLDailyOrder : DLBase
     {
+        private MySqlConnection _connection { get; set; }
+        public DLDailyOrder()
+        {
+            _connection = new MySqlConnection(ConfigUtil.GetAppSettings<string>(AppSettingKeys.ConnectionString));
+
+        }
+
+        public List<DailyOrder> GetDailyOrderByCustomer(long customerId)
+        {
+            return ExecuteReader<DailyOrder>("Proc_GetDailyOrderByCustomer", new { customerId });
+
+
+        }
     }
 }
