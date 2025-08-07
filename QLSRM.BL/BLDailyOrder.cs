0618f32d7a9824be47cefdf87663f6f59df73b62
YEN-XINH\Administrator <haiiyenn210@gmail.com>
2025-08-07 20:30:37 +0700

    .

--- QLSRM.BL/BLDailyOrder.cs
+++ QLSRM.BL/BLDailyOrder.cs
@@ -1,10 +1,12 @@
 ﻿using QLSRM.DL;
+using QLSRM.Library;
 using QLSRM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace QLSRM.BL
 {
@@ -20,6 +22,96 @@ namespace QLSRM.BL
             return _dlDaiLyOrder.GetDailyOrderByCustomer(customerId);
         }
 
-        
+        public override void AfterSaveData<T>(List<T> datas)
+        {
+            try
+            {
+                base.AfterSaveData(datas);
+                if (datas?.Count > 0)
+                {
+                    var customerList = new List<Customer>();
+                    var orderList = new List<DailyOrder>();
+                    var deviveryHistory = new List<DeliveryHistory>();
+                    foreach (var o in datas)
+                    {
+                        DailyOrder orderDaily = Common.Commonfunc.CastToSpecificType<DailyOrder>(o);
+
+                        if (orderDaily != null && orderDaily.EditMode == EditMode.Update)
+                        {
+                            if (orderDaily.Status == (int)OrderStatus.CancelOrder)
+                            {
+                                var customer = _dlBase.GetById<Customer>(orderDaily.CustomerId);
+
+                                if (customer.OrderType == 1)
+                                {
+                                    var newDeliveryDate = customer.EndDate.AddDays(1);
+                                    customer.EndDate = newDeliveryDate;
+                                    customer.EditMode = EditMode.Update;
+                                    customerList.Add(customer);
+                                    var orderCode = _dlBase.SelectNewCode<string>("DailyOrder");
+                                    orderList.Add(new DailyOrder()
+                                    {
+                                        CustomerId = customer.Id,
+                                        OrderCode = orderCode,
+                                        ComboId = customer.ComboId,
+                                        ProvinceId = customer.ProvinceId,
+                                        CommuneId = customer.CommuneId,
+                                        DistrictId = customer.DistrictId,
+                                        Address = customer.Address,
+                                        PhoneNumber = customer.PhoneNumber,
+                                        OrderType = customer.OrderType,
+                                        Quantity = orderDaily.Quantity,
+                                        Status = (int)OrderStatus.WaitDelivery,
+                                        Note = customer.Note,
+                                        Price = orderDaily.Price,
+                                        DeliveryDate = newDeliveryDate,
+                                        EditMode = EditMode.Add,
+                                    });
+                                }
+
+                            }
+                            if (orderDaily.Status == (int)OrderStatus.SuccessfulDelivery)
+                            {
+                                deviveryHistory.Add(new DeliveryHistory()
+                                {
+                                    OrderId = orderDaily.Id,
+                                    Price = orderDaily.Price,
+                                    DeliveryDate = orderDaily.DeliveryDate,
+                                    OrderCode = orderDaily.OrderCode,
+                                    OrderType = orderDaily.OrderType,
+                                    Quantity = orderDaily.Quantity,
+                                    Status = (int)OrderStatus.SuccessfulDelivery,
+                                    EditMode = EditMode.Add
+                                });
+
+                                if (deviveryHistory.Count > 0)
+                                {
+                                    SaveData(deviveryHistory);
+                                }
+
+                            }
+                        }
+
+                    }
+
+                    if (customerList?.Count > 0)
+                    {
+                        SaveData(customerList);
+                    }
+                    if (orderList?.Count > 0)
+                    {
+                        SaveData(orderList);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi lưu dữ liệu DailyOrder: " + ex);
+                throw; // giữ nguyên stack trace gốc
+
+            }
+        }
+
+
     }
 }
