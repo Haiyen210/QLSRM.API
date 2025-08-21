7fcbcfaf017fd836d550bbc8cc2436d9f3ec9084
YEN-XINH\Administrator <haiiyenn210@gmail.com>
2025-08-21 17:35:41 +0700

    .

--- QLSRM.Models/DeliveryAssignments.cs
+++ QLSRM.Models/DeliveryAssignments.cs
@@ -11,6 +11,14 @@ namespace QLSRM.Models
         public long OrderId { get; set; }
         public string OrderCode { get; set; }
         public long AccountId { get; set; }
+        public string AccountName { get; set; }
+        public string CustomerName { get; set; }
+        public string DistrictName { get; set; }
+        public string ProvinceName { get; set; }
+        public string CommuneName { get; set; }
+        public string Address { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Price { get; set; }
         public DateTime? AssignedAt { get; set; }
         public int DeliveryStatus { get; set; }
         public DateTime? DeliveredAt { get; set; }//Thời gian giao hàng thực tế
